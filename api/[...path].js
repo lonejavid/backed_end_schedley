c876aaca4e8311064ae02e263d6fa3f9830bf5c4
SDE-1 Junaid Khan <jk1240651@gmail.com>
2026-03-15 20:26:53 +0530

    deployment fixes

--- api/[...path].js
+++
@@ -1,3 +0,0 @@
-// Vercel serverless: forward all /api/* requests to the Nest app
-// Build must run first so dist/main.js exists (npm run build)
-module.exports = require('../dist/main.js');
--- api/index.js
+++
@@ -1,2 +0,0 @@
-// Handle exact /api – same Nest handler as /api/*
-module.exports = require('../dist/main.js');
